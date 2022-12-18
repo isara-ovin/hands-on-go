7870a30b480ad724f587d57601227a9fed0fe41a
Isara Ovin <isaraovin@gmail.com>
2022-12-19 07:36:28 +0800

    Packages

--- packages/imports/begin/main.go
+++ packages/imports/begin/main.go
@@ -1,12 +1,18 @@
 // packages/imports/begin/main.go
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 // import the fmt package from the standard library
 
 // import the time package from the standard library
 
 func main() {
 	// use the fmt package to print the string "Hello Gopher!"
-
+	fmt.Println("Hello Gophers")
 	// use the time package to print the current weekday
+	fmt.Println(time.Now().Weekday())
 }
