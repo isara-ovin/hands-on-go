0e6935e32153e60157bb5aeb1f4e1eb45453c86a
Isara Ovin <isaraovin@gmail.com>
2022-12-19 09:27:20 +0800

    Struct Basics

--- types/structs/fields/begin/main.go
+++ types/structs/fields/begin/main.go
@@ -1,13 +1,22 @@
 // types/structs/fields/begin/main.go
 package main
 
+import "fmt"
+
 // define a struct type for author
-//
+type author struct {
+	firstName string
+	lastName  string
+}
 
 func main() {
 	// intialize author
-	//
+	a := author{
+		firstName: "Isara",
+		lastName:  "Ovin",
+	}
 
 	// print the author
 	// fmt.Printf("%#v\n", a)
+	fmt.Printf("%#v", a)
 }
